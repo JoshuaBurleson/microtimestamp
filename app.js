55490121f551ec886058ed2d34d82842b4096933
Joshua Burleson <Joshua Burleson>
2017-05-31 02:17:40 -0400

    port fix

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ var index = app.get('/',function(req,res){
     fs.readFile('./index.html',function(err, data){
         if(err){throw err}
         res.write(data);
-    })
-    res.end()
+        res.end();
+    });
+    
 });
 
 var timechange = app.get(exp,function(req, res){
